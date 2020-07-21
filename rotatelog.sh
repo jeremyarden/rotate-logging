ef6328c5981198bc087004de2df7cbf0bdd96b4d
Jeremy Arden <ardenjeremy@gmail.com>
2020-07-21 11:48:32 +0700

    added case statement for rotation

--- rotatelog.sh
+++ rotatelog.sh
@@ -6,13 +6,35 @@ current_date=$(date +'%m-%d-%Y')
 file_size=$(stat -f %z "$file_name")
 
 rotate() {
+    getLatestVer
+
+    echo $latest
+
     if [ "$file_size" -ge "$log_size" ]
     then
-        zip -v "${file_name%%.*}-${current_date}".zip "$file_name"
+        case ${latest_ver} in
+        "")
+            echo "null situation"
+            zip -v "${file_name%%.*}-${current_date}".zip "$file_name"
+            ;;
+        zip)
+            echo "zip situation"
+            mv "${file_name%%.*}-${current_date}".zip "${file_name%%.*}-${current_date}".zip.1
+            zip -v "${file_name%%.*}-${current_date}".zip.2 "$file_name"
+            ;;
+        [1-9]*)
+            echo "${latest_ver} situation"
+            latest_ver=$((latest_ver + 1))
+            zip -v "${file_name%%.*}-${current_date}".zip."${latest_ver}" "$file_name"
+            ;;
+        esac
     fi
 }
 
-getMaxVer() {
-    
+getLatestVer() {
+    latest=$(ls -tr "${file_name%%.*}-${current_date}".zip* | tail -n 1)
+    latest_ver=${latest##*.}
+    echo ${latest##*.}
 }
 
+rotate
