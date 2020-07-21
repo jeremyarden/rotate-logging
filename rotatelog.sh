49eeb975bfb678eeb8eaaf9f81c15c85717c849e
Jeremy Arden <ardenjeremy@gmail.com>
2020-07-21 10:09:31 +0700

    added rotate function

--- rotatelog.sh
+++ rotatelog.sh
@@ -8,26 +8,11 @@ file_size=$(stat -f %z "$file_name")
 rotate() {
     if [ "$file_size" -ge "$log_size" ]
     then
-        max_ver="$(getMaxVer)"
-
-        if [[ "$max_ver" -eq "-1"]]
-        then
-            zip "${file_name}-${current_date}".zip "${file_name}"
-        else
-            zip "${file_name}-${current_date}".zip "${file_name}"
+        zip -v "${file_name%%.*}-${current_date}".zip "$file_name"
+    fi
 }
 
 getMaxVer() {
-    max=-1
-    pattern="${file_name}-${current_date}.zip."
-
-    if 
-
-    for file in $(find . -name "${pattern}*")
-    do
-        num=${file##pattern}
-        [[ "$num" -gt "$max" ]] && $max=$num
-    done
-
-    echo $max
+    
 }
+
