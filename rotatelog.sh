1cf75157b0712c446b2736e020facbabe4f95561
Jeremy Arden <ardenjeremy@gmail.com>
2020-07-21 12:49:51 +0700

    added file removal function

--- rotatelog.sh
+++ rotatelog.sh
@@ -5,25 +5,24 @@ log_size=$2
 current_date=$(date +'%m-%d-%Y')
 file_size=$(stat -f %z "$file_name")
 
+removeFileOlderThan() {
+    find . -mtime +30 -exec rm {} \;
+}
+
 rotate() {
     getLatestVer
-
-    echo $latest
 
     if [ "$file_size" -ge "$log_size" ]
     then
         case ${latest_ver} in
         "")
-            echo "null situation"
             zip -v "${file_name%%.*}-${current_date}".zip "$file_name"
             ;;
         zip)
-            echo "zip situation"
             mv "${file_name%%.*}-${current_date}".zip "${file_name%%.*}-${current_date}".zip.1
             zip -v "${file_name%%.*}-${current_date}".zip.2 "$file_name"
             ;;
         [1-9]*)
-            echo "${latest_ver} situation"
             latest_ver=$((latest_ver + 1))
             zip -v "${file_name%%.*}-${current_date}".zip."${latest_ver}" "$file_name"
             ;;
@@ -34,7 +33,6 @@ rotate() {
 getLatestVer() {
     latest=$(ls -tr "${file_name%%.*}-${current_date}".zip* | tail -n 1)
     latest_ver=${latest##*.}
-    echo ${latest##*.}
 }
 
 rotate
